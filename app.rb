5033185c416301718e884e12f6ee1a13a9a895cf
furu <ba09219@gmail.com>
2014-03-19 03:40:13 +0900

    まだ Sinatra 用になっていない部分があるかも

--- app.rb
+++ app.rb
@@ -4,8 +4,40 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'slim'
 require 'rack-flash'
+require 'sinatra/cookies'
 
 require_relative 'models/user'
+
+module SessionsHelper
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+end
 
 configure :development do
   require 'sinatra/reloader'
@@ -17,6 +49,7 @@ configure do
 end
 
 use Rack::Flash
+helpers SessionsHelper
 
 get '/' do
   slim :index
@@ -41,6 +74,8 @@ end
 
 # セッションを削除する (サインアウト)
 delete '/sighout' do
+  sign_out
+  redirect '/'
 end
 
 __END__
