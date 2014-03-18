515032836af42a16a8b1504ab73a7ee58bfca1ce
furu <ba09219@gmail.com>
2014-03-18 18:26:37 +0900

    たしか7章をとばして8章をやっててその途中

--- app.rb
+++ app.rb
@@ -11,9 +11,33 @@ configure :development do
   require 'pry'
 end
 
+configure do
+  enable :sessions
+end
+
 
 get '/' do
-  'hi'
+  slim :index
+end
+
+# 新しいセッション用 (サインイン)
+get '/signin' do
+  slim :signin
+end
+
+# 新しいセッションを作成する
+post '/sessions' do
+  user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    sign_in user
+    redirect '/'
+  else
+    slim :signin
+  end
+end
+
+# セッションを削除する (サインアウト)
+delete '/sighout' do
 end
 
 __END__
--- models/user.rb
+++ models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   before_save { email.downcase! }
+  before_create :create_remember_token
 
   # 存在性、長さ、フォーマット、一意性の検証
   #
@@ -18,4 +19,17 @@ class User < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
